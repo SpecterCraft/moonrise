21a8751dc95f0ad0668a08b32d96847e28ac0ebd
hadasfada <68920407+hadasfada@users.noreply.github.com>
2022-08-20 23:42:39 +0300

    Update serverstatus-home.js

--- docs/assets/serverstatus-home.js
+++ docs/assets/serverstatus-home.js
@@ -1,18 +1,28 @@
----
----
-{% include serverstatus.js %}
+// Handling errors
+function handleErrors(response) {
+    if (!response.ok) {
+        document.getElementById("serverStatus").innerHTML = "";
+        document.getElementById("serverStatusM").innerHTML = "";
+        document.getElementById("serverLogoName").classList.remove("hidden");
+    }
+    return response;
+}
+
+// Fetch API, mcsrvstat
+fetch("https://api.mcsrvstat.us/2/31.41.59.26:5358")
+    .then(handleErrors)
+    .then(response => {
+        return response.json();
+    })
 .then(data => {
     if (data.online == true) {
         document.getElementById("serverVer").innerHTML = `Minecraft ${data.version} Server`;
 
         // siteNav dependent
-        {% if site.siteNav %}
-        document.getElementById("serverInd").classList.add("text-green-600");
-        document.getElementById("serverPlaynow").innerHTML = `<span class="inline-block bg-yellow-500 rounded-full px-3 py-1 text-sm font-thin uppercase text-gray-900 mr-2 shadow-lg">Play Now: ${data.players.online} Online</span>`;
-        {% else %}
+        
         document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;We're online!&ensp;&middot;&ensp;${data.players.online} playing</p>`;
         document.getElementById("serverStatusM").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;Online, ${data.players.online} playing</p>`;
-        {% endif %}
+        
     }
     else {
         document.getElementById("serverPlaynow").innerHTML = `<span class="inline-block bg-red-500 rounded-full px-3 py-1 text-sm font-thin uppercase text-gray-900 mr-2 shadow-lg">Currently Offline</span>`;
@@ -20,11 +30,9 @@
         document.getElementById("serverIP").classList.add("bg-orange-800");
 
         // siteNav dependent
-        {% if site.siteNav %}
-        document.getElementById("serverInd").classList.add("text-red-600");
-        {% else %}
+        
         document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-red-600">&#11044;</span>&emsp;Sorry! We're offline...</p>`;
         document.getElementById("serverStatusM").innerHTML = `<p class="font-bold select-none"><span class="text-red-600">&#11044;</span>&emsp;Offline</p>`;
-        {% endif %}
+        
     }
 });
